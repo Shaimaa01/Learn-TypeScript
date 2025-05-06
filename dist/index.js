83dc5c12cfd6662beaf88daeb92579708800e07e
shaimaa <shisko939@gmail.com>
2025-05-06 12:50:26 +0200

    video 8,9,10,11

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log(Math.floor(10));
+let theName = "Elzero";
+let theAge = 40;
+let hire = true;
+let all;
+theName = "shaimaa";
+all = 100;
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(10, 20));
+console.log(typeof add(10, 20));
+let all2 = "osama";
+all2 = "A";
+all2 = 100;
+all2 = true;
+let myFriends = ["osama", "Ahmed", "Sayed"];
+for (let i = 0; i < myFriends.length; i++) {
+    console.log(myFriends[i].repeat(3));
+}
+let arrayFour = [1, 2, 3, 4, "A", "B", ["C", "D"], true, false];
+let showMsg = true;
+function showDetails(name, age, salary) {
+    let test = 10;
+    if (showMsg) {
+        return `Hello ${name}, Age Is ${age}, Salary Is ${salary} , Test Variable ${test}`;
+    }
+    return `Nothing here ! Go to play football `;
+}
+console.log(showDetails("Osama", 40, 5000));
+function showData(name, age = 60, country) {
+    return `${name} - ${age} - ${country}`;
+}
+console.log(showData("Osama", 40));
+function addAll(...nums) {
+    let result = 0;
+    nums.forEach((num) => result += num);
+    return result;
+}
+console.log(addAll(10, 20, 30, 10.5, +true));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,80 @@ for(let i=0; i<myFriends.length; i++){
 // Type Annotations With Multidimensional Arrays
 
  let arrayFour:(string | number | string[] | boolean)[] = [1,2,3,4, "A" , "B" , ["C" , "D"], true , false];
+
+ /*
+  Type  Annotations With Functions 
+
+    noImplicitAny
+
+    noImplicitReturns 
+      Will chek All code Paths In AFunction To Ensure They Return A Value
+
+    noUnsedLocals
+      Report Errors On Unused Local Variables
+
+    noUnusedParameters
+      Report Errors On Unused Parameters In Functions.
+ */
+
+let showMsg = true;
+
+function showDetails(name: string,age: number,salary: number) : string{
+  let test = 10;
+  if(showMsg){
+    return `Hello ${name}, Age Is ${age}, Salary Is ${salary} , Test Variable ${test}`;
+  }
+
+  return `Nothing here ! Go to play football `
+}
+
+console.log(showDetails("Osama" , 40 , 5000));
+
+/*
+Function 
+  Optional and Defalut Parameters
+
+  In JavaScript , Every Parameter Is Optional => "undefined" If You Didn`t Use It 
+  "?" Optional Parameter
+*/
+
+function showData(name?: string ,age: number= 60, country?: string){
+  return `${name} - ${age} - ${country}`
+}
+
+console.log(showData("Osama" , 40 ));
+
+/*
+  Function
+    Rest Parameter
+    What About Float => All Is Under Type Number
+*/
+
+function addAll(...nums:number[]) : number{
+let result = 0;
+// for(let i =0; i < nums.length; i++){
+//   result += nums[i];
+// }
+
+nums.forEach((num) => result += num);
+
+return result;
+}
+
+console.log(addAll(10,20,30,10.5, +true));
+
+/*
+  Function
+  Anonymous Function
+  Arrow Function
+*/
+
+const addTwo = function(num1:number, num2:number): number{
+  return num1 + num2;
+}
+
+console.log(addTwo(10,20));
+
+const addWithArrow = (num1:number , num2:number) : number => num1 + num2 ;
+
+console.log(addWithArrow(10,20));
